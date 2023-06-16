dde3ace747332604fa425dc2a171df8bf945af02
KofiRomio <dotsemawufemorromio@gmail.com>
2023-06-16 01:14:01 -0700

    Your program should return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
 * main - A program that prints a line to the standard error
-* Return: 0 (success)
+* Return: 0(error)
 */
 int main(void)
 {
-write(2,"and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
