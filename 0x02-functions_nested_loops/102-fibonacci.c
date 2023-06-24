e1dca68558cc50a61be1e8e2afeca0ce0a902dd5
KofiRomio <dotsemawufemorromio@gmail.com>
2023-06-24 01:24:46 -0700

    a program that finds and prints the sum of the even-valued terms, followed by a new line

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
 *main - entry point
-*Description: prints the sum of even-valued
-*Fibonacci sequence not exceed 4000000
+*Description: prints the first 50
+*Fibonacci numbers starting with
+*1 and 2 followed by a new line
 *Return: Always 0 (success)
 */
 int main(void)
 {
+int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
-float total_sum;
-while (1)
+for (count = 0; count < 50; count++)
 {
 sum = fib1 + fib2;
-if (sum > 4000000)
-break;
-if ((sum % 2) == 0)
-total_sum += sum;
 printf("%lu", sum);
 fib1 = fib2;
 fib2 = sum;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
 }
-printf("%.0f\n", total_sum);
 return (0);
 }
