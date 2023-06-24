25ba1df1186ad9ff58c07f1905d3755238ac3bb9
KofiRomio <dotsemawufemorromio@gmail.com>
2023-06-23 22:45:56 -0700

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.C
+++ 0x02-functions_nested_loops/100-times_table.C
@@ -7,7 +7,7 @@
 void print_times_table(int n)
 {
 int prod, mult, num;
-if    ( n <= 15 && n >= 0)
+if (n <= 15 && n >= 0)
 {
 for (num =0; num <= n; ++num)
 {
@@ -21,12 +21,12 @@ if (prod <= 9)
 _putchar(' ');
 if (prod <= 99)
 _putchar(' ');
-if(prod >= 100)
+if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
 _putchar(((prod / 10)) % 10 + 48);
 }
-else if    ( prod <= 99 && prod >= 10)
+else if ( prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + 48);
 }
